2bd8bc491a49fbb822e4eac46614b452076cfc66
josiqueli <105284716+josiqueli@users.noreply.github.com>
2024-09-19 07:57:47 -0300

    ok

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,7 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+console.log(url);
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+
+}
