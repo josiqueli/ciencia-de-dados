8c73e1d02ac9620d4b8cc17b499804af9f898cab
josiqueli <105284716+josiqueli@users.noreply.github.com>
2024-10-03 07:59:43 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,15 +4,16 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     cosole.log(dados);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${pessoasConectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-   
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+
+
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)  
 }
   
   vizualizarInformacoesGlobais()
-  
   
