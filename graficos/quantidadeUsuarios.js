b65dc1aa229a31035b30c84932a171832be31dbf
josiqueli <105284716+josiqueli@users.noreply.github.com>
2024-10-17 07:37:12 -0300

    k

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,8 +1,25 @@
+https://raw.githubusercontent.com/josiqueli/ciencia-de-dados/refs/heads/main/base-de-dados/educacao-dados-globais.json
 
 async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-      
-      console.log(dados)
+  const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+  const nomeDasRedes = Object.keys(dados)
+  const quantidadeUsuarios = Object.values(dados)
+
+const data = [
+  {
+    x: nomeDasRedes,
+    y: quantidadeUsuarios,
+    type: 'bar'
   }
+]
+
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
+
+}
+
+quantidadeUsuarios()
