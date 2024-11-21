61e71060a7429b2c0c2b0ecd01c2e7f8f21ee415
josiqueli <105284716+josiqueli@users.noreply.github.com>
2024-11-21 08:38:26 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,16 +3,16 @@ const url = 'https://raw.githubusercontent.com/josiqueli/ciencia-de-dados/refs/h
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_com_acesso_a_educacao / 6.0e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 7.888e9)
+    const pessoasEducacao = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_dia_estudando)
-    const pessoasEnsinoS = (dados.total_pessoas_com_educacao_superior / 1.5e9)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasEnsinoS / pessoasNoMundo ) * 100).toFixed(2)
+    const pessoasEnsinoS = (dados.total_pessoas_com_educacao_superior / 1e9)
+    //const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemEstudando = ((pessoasEducacao/ pessoasNoMundo ) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_com_acesso_a_educacao} bilhões</span> tem acesso à alguma rede de educação e passam em média <span>${horas} horas</span> estudando.<br>Desse total, apenas <span>${porcentagemConectada}%</span> de pessoas tem ensino superior.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_com_acesso_a_educacao} bilhões</span> tem acesso à alguma rede de educação e passam em média <span>${horas} horas</span> estudando.<br>Desse total, apenas <span>${pessoasEnsinoS}%</span> de pessoas tem ensino superior. Portanto, cerca de <span>${porcentagemEstudando}%</span> pessoasestã estudando.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
