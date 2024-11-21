f851b9dc210a5aa13ab7245b669bccf07f1c68cf
josiqueli <105284716+josiqueli@users.noreply.github.com>
2024-11-21 08:39:47 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,8 +3,8 @@ const url = 'https://raw.githubusercontent.com/josiqueli/ciencia-de-dados/refs/h
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasEducacao = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const pessoasEducacao = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
     const horas = parseInt(dados.tempo_medio_dia_estudando)
     const pessoasEnsinoS = (dados.total_pessoas_com_educacao_superior / 1e9)
     //const minutos = Math.round((dados.tempo_medio - horas) * 100)
@@ -12,7 +12,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_com_acesso_a_educacao} bilhões</span> tem acesso à alguma rede de educação e passam em média <span>${horas} horas</span> estudando.<br>Desse total, apenas <span>${pessoasEnsinoS}%</span> de pessoas tem ensino superior. Portanto, cerca de <span>${porcentagemEstudando}%</span> pessoasestã estudando.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_com_acesso_a_educacao} bilhões</span> tem acesso à alguma rede de educação e passam em média <span>${horas} horas</span> estudando.<br>Desse total, apenas <span>${pessoasEnsinoS}%</span> de pessoas tem ensino superior. Portanto, cerca de <span>${porcentagemEstudando}%</span> pessoas estão estudando.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
