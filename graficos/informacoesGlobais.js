211e875e0b8d1741ab892e92590cde2410cb3891
josiqueli <105284716+josiqueli@users.noreply.github.com>
2024-11-21 08:29:12 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,12 +6,13 @@ async function vizualizarInformacoesGlobais() {
     const pessoasConectadas = (dados.total_pessoas_com_acesso_a_educacao / 6.0e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 7.888e9)
     const horas = parseInt(dados.tempo_medio_dia_estudando)
+    const pessoasEnsinoS = (dados.total_pessoas_com_educacao_superior / 1.5e9)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+    const porcentagemConectada = ((pessoasEnsinoS / pessoasNoMundo ) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_com_acesso_a_educacao} bilhões</span> tem acesso à alguma rede de educação e passam em média <span>${horas} horas</span> estudando.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_com_acesso_a_educacao} bilhões</span> tem acesso à alguma rede de educação e passam em média <span>${horas} horas</span> estudando.<br>Desse total, apenas <span>${porcentagemConectada}%</span> de pessoas tem ensino superior.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
